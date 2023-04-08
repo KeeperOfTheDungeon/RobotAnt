d14aefd9a75d7720fbc7c3beecbbf5a0ba786b16
ermu1013 <ermu1013@gmail.com>
2023-04-08 13:10:28 +0200

    import parameter handling via argparse from the spiderbot fork

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-# github Bastik00
-from AntView.AntView import AntView
+#!/usr/bin/env python
+import argparse
+
 from ant import Ant
+from AntView.AntView import AntView
 
 
+def main(do_gui: bool, do_cli: bool):
+    ant = Ant()
+    if do_gui:
+        _ant_viewer = AntView(ant)
+    if do_cli:
+        ant.run()
 
 
-ant  = Ant()
-ant_viewer = AntView(ant)
-ant.run()
+def parse_args():
+    parser = argparse.ArgumentParser(description='GUI/CLI for the Ant robot from HsKa.')
+    parser.add_argument('--gui', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--cli', action=argparse.BooleanOptionalAction)
+    parser.set_defaults(gui=True, cli=False)
+    return parser.parse_args()
 
 
-print("end")
+if __name__ == "__main__":
+    args = parse_args()
+    main(args.gui, args.cli)
+    print("Shutting down!")
