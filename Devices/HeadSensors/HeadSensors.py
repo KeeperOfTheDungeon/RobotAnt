6291fa6e3f2374ea0fe63840151fc6b9ea645064
ermu1013 <ermu1013@gmail.com>
2023-06-20 17:55:47 +0200

    KW25-1 reformat the relevant files - HeadSensors - LegController - LegSensors

--- Devices/HeadSensors/HeadSensors.py
+++ Devices/HeadSensors/HeadSensors.py
@@ -3,51 +3,36 @@ from Devices.HeadSensors.HeadSensorsVcnl4020Set import HeadSensorsVcnl4020Set
 from RoboControl.Robot.Device.RobotDevice import RobotDevice
 
 
-
 class HeadSensors(RobotDevice):
-	
 
-	
-	def __init__(self, component_config):
-		super().__init__(component_config)
-		
+    def __init__(self, component_config):
+        super().__init__(component_config)
 
-	def build(self):
-		self._protocol = HeadSensorsProtocol(self)
+    def build(self):
+        self._protocol = HeadSensorsProtocol(self)
 
-		protocol = self._protocol.get_vcnl4000_protocol()
-		self._vcnl_4020_set = HeadSensorsVcnl4020Set(protocol)
-		self.add_components(self._vcnl_4020_set)
+        protocol = self._protocol.get_vcnl4000_protocol()
+        self._vcnl_4020_set = HeadSensorsVcnl4020Set(protocol)
+        self.add_components(self._vcnl_4020_set)
 
-		self.build_protocol()
+        self.build_protocol()
 
+    def build_protocol(self):
+        super().build_protocol()
+        self._remote_command_processor_list.extend(self._vcnl_4020_set.get_command_processors())
+        self._remote_message_processor_list.extend(self._vcnl_4020_set.get_message_processors())
+        self._remote_stream_processor_list.extend(self._vcnl_4020_set.get_stream_processors())
 
+    def get_data_aquisators(self):
+        aquisators = super().get_data_aquisators()
+        aquisators.extend(["Mxl90614 ambient temperature", "Mxl90614 object temperature", "lux", "distance"])
+        return aquisators
 
-	def build_protocol(self):	
-		super().build_protocol()
-		self._remote_command_processor_list.extend(self._vcnl_4020_set.get_command_processors())
-		self._remote_message_processor_list.extend(self._vcnl_4020_set.get_message_processors())
-		self._remote_stream_processor_list.extend(self._vcnl_4020_set.get_stream_processors())
+    def get_vcnl_4000_set(self):
+        return self._vcnl_4020_set
 
-		
-
-
-
-	def get_data_aquisators(self):
-		aquisators = super().get_data_aquisators()
-		aquisators.extend(["Mxl90614 ambient temperature" ,"Mxl90614 object temperature",  "lux", "distance"])
-		return aquisators
-
-
-
-	def get_vcnl_4000_set(self):
-		return self._vcnl_4020_set
-
-	def _load_setup(self):
-		self._vcnl_4020_set.load_settings()
-
-
-	
+    def _load_setup(self):
+        self._vcnl_4020_set.load_settings()
 
 
 """package de.hska.lat.ant_IIIb.devices.headSensors;
@@ -79,51 +64,51 @@ import de.hska.lat.robot.device.RobotDevice;
 
 public class HeadSensors extends RobotDevice<DeviceEventNotifier, HeadSensorsProtocol >
 {
-	
-	/**
-	 * @uml.property  name="servos"
-	 */
-	protected AnalogServoServoSet	servos;
-	
-	/**
-	 * @uml.property  name="vcnl4000set"
-	 * @uml.associationEnd  multiplicity="(1 1)"
-	 */
-	protected HeadSensorsVcnl4000Set vcnl4000set;
-	
-	/**
-	 * @uml.property  name="mxl90614Set"
-	 */
-	protected HeadMlx90614Set mxl90614Set;
-	
-	/**
-	 * @uml.property  name="bmp085SensorSet"
-	 * @uml.associationEnd  multiplicity="(1 1)"
-	 */
-	protected Bmp085Set bmp085SensorSet;
-	
-	
-	
+    
+    /**
+     * @uml.property  name="servos"
+     */
+    protected AnalogServoServoSet	servos;
+    
+    /**
+     * @uml.property  name="vcnl4000set"
+     * @uml.associationEnd  multiplicity="(1 1)"
+     */
+    protected HeadSensorsVcnl4000Set vcnl4000set;
+    
+    /**
+     * @uml.property  name="mxl90614Set"
+     */
+    protected HeadMlx90614Set mxl90614Set;
+    
+    /**
+     * @uml.property  name="bmp085SensorSet"
+     * @uml.associationEnd  multiplicity="(1 1)"
+     */
+    protected Bmp085Set bmp085SensorSet;
+    
+    
+    
 public HeadSensors(DeviceMetaData metaData)
 {
-	super(metaData);
-	
-	
+    super(metaData);
+    
+    
 
-	this.aquisators = HeadSensorsDataAquisator.aquisators;
-	
+    this.aquisators = HeadSensorsDataAquisator.aquisators;
+    
 //	this.addBmp085Sensors(metaData);
 
-	
-	this.mxl90614Set = new HeadMlx90614Set(HeadSensorsProtocol.getMlx90614Protocol(this.getId()));
-	this.addComponentSet(this.mxl90614Set );
-	
-	this.vcnl4000set = new HeadSensorsVcnl4000Set(HeadSensorsProtocol.getVcnl4000Protocol(this.getId()));
-	this.addComponentSet(this.vcnl4000set );
-	
-	
-	
-	this.protocol=new HeadSensorsProtocol(this);
+    
+    this.mxl90614Set = new HeadMlx90614Set(HeadSensorsProtocol.getMlx90614Protocol(this.getId()));
+    this.addComponentSet(this.mxl90614Set );
+    
+    this.vcnl4000set = new HeadSensorsVcnl4000Set(HeadSensorsProtocol.getVcnl4000Protocol(this.getId()));
+    this.addComponentSet(this.vcnl4000set );
+    
+    
+    
+    this.protocol=new HeadSensorsProtocol(this);
 
 }
 
@@ -135,9 +120,9 @@ public HeadSensors(DeviceMetaData metaData)
 /*
 protected void addBmp085Sensors(HeadSensorsMetaData metaData)
 {
-	
-	this.bmp085SensorSet = new Bmp085Set(metaData.getBmp085data(),HeadSensorsProtocol.getBmp085Protocol(this.getId()));
-	this.addComponentSet(this.bmp085SensorSet);
+    
+    this.bmp085SensorSet = new Bmp085Set(metaData.getBmp085data(),HeadSensorsProtocol.getBmp085Protocol(this.getId()));
+    this.addComponentSet(this.bmp085SensorSet);
 }
 
 */
@@ -145,33 +130,33 @@ protected void addBmp085Sensors(HeadSensorsMetaData metaData)
 
 public void loadSetup()
 {
-	
-	
-	this.mxl90614Set.loadSettings();
-	this.vcnl4000set.loadSettings();
+    
+    
+    this.mxl90614Set.loadSettings();
+    this.vcnl4000set.loadSettings();
 //	this.bmp085SensorSet.loadSettings();
-	
-	
+    
+    
 }
 
 public Mlx90614Set getMlx90614Set()
 {
-	return(this.mxl90614Set);
-	
+    return(this.mxl90614Set);
+    
 }
 
 
 
 public HeadSensorsVcnl4000Set getVcnl4000Set()
 {
-	return (this.vcnl4000set);
+    return (this.vcnl4000set);
 }
 
 
 
 public Bmp085Set getBmp085Set()
 {
-	return (this.bmp085SensorSet);
+    return (this.bmp085SensorSet);
 }
 
 
@@ -180,23 +165,23 @@ public Bmp085Set getBmp085Set()
 @Override
 public ArrayList<ComponentValue<?>> getInputValues()
 {
-	ArrayList<ComponentValue<?>> values = new ArrayList<ComponentValue<?>>();
+    ArrayList<ComponentValue<?>> values = new ArrayList<ComponentValue<?>>();
 
-	
-	for (Mlx90614 sensor: this.mxl90614Set)
-	{
-		values.add(sensor.getAmbientValue());
-		values.add(sensor.getObjectValue());
-	}
-	
-	
+    
+    for (Mlx90614 sensor: this.mxl90614Set)
+    {
+        values.add(sensor.getAmbientValue());
+        values.add(sensor.getObjectValue());
+    }
+    
+    
 
-	values.addAll(this.bmp085SensorSet.getValues());
-	
-	values.addAll(this.mxl90614Set.getValues());
-	
-	values.addAll(this.vcnl4000set.getValues());
-	return(values);
+    values.addAll(this.bmp085SensorSet.getValues());
+    
+    values.addAll(this.mxl90614Set.getValues());
+    
+    values.addAll(this.vcnl4000set.getValues());
+    return(values);
 }
 
 */
@@ -207,7 +192,7 @@ public ArrayList<ComponentValue<?>> getInputValues()
  */
 public AnalogServoServoSet getServos()
 {
-	return (this.servos);
+    return (this.servos);
 }
 
 
--- Devices/LegController/LegController.py
+++ Devices/LegController/LegController.py
@@ -1,4 +1,3 @@
-
 from Devices.LegController.LegControllerCurrentSensors import LegControllerCurrentSensors
 from Devices.LegController.LegControllerProtocol import LegControllerProtocol
 from Devices.LegController.LegControllerServoSet import LegControllerServos
@@ -8,60 +7,52 @@ from RoboControl.Robot.Device.RobotDevice import RobotDevice
 
 
 class LegController(RobotDevice):
-	
-	
-	def __init__(self, component_config):
-		super().__init__(component_config)
 
+    def __init__(self, component_config):
+        super().__init__(component_config)
 
-	def build(self):
-		self._protocol = LegControllerProtocol(self)
+    def build(self):
+        self._protocol = LegControllerProtocol(self)
 
-		protocol = self._protocol.get_servo_protocol()
-		self._servo_set = LegControllerServos(protocol)
-		self.add_components(self._servo_set)
+        protocol = self._protocol.get_servo_protocol()
+        self._servo_set = LegControllerServos(protocol)
+        self.add_components(self._servo_set)
 
-		protocol = self._protocol.get_current_protocol()
-		self._current_sensor_set = LegControllerCurrentSensors(protocol)
-		self.add_components(self._current_sensor_set)
+        protocol = self._protocol.get_current_protocol()
+        self._current_sensor_set = LegControllerCurrentSensors(protocol)
+        self.add_components(self._current_sensor_set)
 
+        self.build_protocol()
 
-		self.build_protocol()
+    def build_protocol(self):
+        super().build_protocol()
 
+        self._remote_command_processor_list.extend(self._servo_set.get_command_processors())
+        self._remote_command_processor_list.extend(self._current_sensor_set.get_command_processors())
 
-	def build_protocol(self):
-		super().build_protocol()
-		
-		self._remote_command_processor_list.extend(self._servo_set.get_command_processors())
-		self._remote_command_processor_list.extend(self._current_sensor_set.get_command_processors())
+        self._remote_message_processor_list.extend(self._servo_set.get_message_processors())
+        self._remote_message_processor_list.extend(self._current_sensor_set.get_message_processors())
 
-		self._remote_message_processor_list.extend(self._servo_set.get_message_processors())
-		self._remote_message_processor_list.extend(self._current_sensor_set.get_message_processors())
-		
-		self._remote_stream_processor_list.extend(self._servo_set.get_stream_processors())
-		self._remote_stream_processor_list.extend(self._current_sensor_set.get_stream_processors())
+        self._remote_stream_processor_list.extend(self._servo_set.get_stream_processors())
+        self._remote_stream_processor_list.extend(self._current_sensor_set.get_stream_processors())
 
+    def get_servo_set(self):
+        return self._servo_set
 
+    def get_current_sensors(self):
+        return self._current_sensor_set
 
-	def get_servo_set(self):
-		return self._servo_set
-
-	
-	def get_current_sensors(self):
-		return self._current_sensor_set
-
-
-	def get_data_aquisators(self):
-		aquisators = super().get_data_aquisators()
-		aquisators.extend(["servo Positionse" ,
-							"Servo Destinations",
-		  					"Servo Status",
-							"current consumption",
-							"current max consumption",
-							"current Total consumption",
-							"servo raw analogue values",
-							"temperatures"])
-		return aquisators
+    def get_data_aquisators(self):
+        aquisators = super().get_data_aquisators()
+        aquisators.extend(["servo Positionse",
+                           "Servo Destinations",
+                           "Servo Status",
+                           "current consumption",
+                           "current max consumption",
+                           "current Total consumption",
+                           "servo raw analogue values",
+                           "temperatures"])
+        return aquisators
 
 
 """package de.hska.lat.ant_IIIb.devices.legController;
@@ -86,47 +77,47 @@ import de.hska.lat.robot.device.RobotDevice;
 public class LegController extends RobotDevice <DeviceEventNotifier, LegControllerProtocol>
 {
 
-	/**
-	 * @uml.property  name="servos"
-	 */
-	
-	protected LegControllerCurrentSensors currentSensors;
-	
-	protected LegControllerServos servos;	
-	
-	protected LegControllerLm75 temperatureSensors;
-	
-	/**
-	 * @uml.property  name="motionController"
-	 * @uml.associationEnd  multiplicity="(1 1)"
-	 */
-	protected AntMotionController motionController;
-	
+    /**
+     * @uml.property  name="servos"
+     */
+    
+    protected LegControllerCurrentSensors currentSensors;
+    
+    protected LegControllerServos servos;	
+    
+    protected LegControllerLm75 temperatureSensors;
+    
+    /**
+     * @uml.property  name="motionController"
+     * @uml.associationEnd  multiplicity="(1 1)"
+     */
+    protected AntMotionController motionController;
+    
 public LegController(DeviceMetaData metaData)
 {
-	super(metaData); 
-	aquisators= LegControllerDataAquisator.aquisators;
+    super(metaData); 
+    aquisators= LegControllerDataAquisator.aquisators;
 
-	this.servos  = new LegControllerServos(LegControllerProtocol.getServoProtocol(this.getId()));
-	this.addComponentSet(this.servos);	
+    this.servos  = new LegControllerServos(LegControllerProtocol.getServoProtocol(this.getId()));
+    this.addComponentSet(this.servos);	
  
-	
-	this.currentSensors  = new LegControllerCurrentSensors(LegControllerProtocol.getCurrentProtocol(this.getId()));
-	this.addComponentSet(this.currentSensors);	
+    
+    this.currentSensors  = new LegControllerCurrentSensors(LegControllerProtocol.getCurrentProtocol(this.getId()));
+    this.addComponentSet(this.currentSensors);	
  
-	
-	this.temperatureSensors  = new LegControllerLm75(LegControllerProtocol.getTemperatureProtocol(this.getId()));
-	this.addComponentSet(this.temperatureSensors);	
+    
+    this.temperatureSensors  = new LegControllerLm75(LegControllerProtocol.getTemperatureProtocol(this.getId()));
+    this.addComponentSet(this.temperatureSensors);	
  
-	
-	this.motionController  = new AntMotionController(AntComponents.MOTION_CONTROLLER.getMetaData(), LegControllerProtocol.getMotionProtocol(this.getId()),this); 
-	this.componentList.add(this.motionController);
-	
-	
-	
-	
-	
-	this.protocol=new LegControllerProtocol(this);
+    
+    this.motionController  = new AntMotionController(AntComponents.MOTION_CONTROLLER.getMetaData(), LegControllerProtocol.getMotionProtocol(this.getId()),this); 
+    this.componentList.add(this.motionController);
+    
+    
+    
+    
+    
+    this.protocol=new LegControllerProtocol(this);
 
 }
 
@@ -140,38 +131,38 @@ public LegController(DeviceMetaData metaData)
  */
 public ServoSet getServos()
 {
-	return (this.servos);
+    return (this.servos);
 }
 
 
 public LegControllerCurrentSensors getCurrentSensors()
 {
-	return(currentSensors);
+    return(currentSensors);
 }
 
 
 public void loadSetup()
 {
-	for (Servo servo : this.servos)
-	{
-		servo.remote_loadDefaults();
-		servo.remote_getSettings();
-		servo.remote_getServoSpeed();
-		servo.remote_getServoStatus();
-	
-	}
-	
-	for (CurrentSensor current :this.currentSensors)
-	{
-		current.remote_getSettings();
-	}
+    for (Servo servo : this.servos)
+    {
+        servo.remote_loadDefaults();
+        servo.remote_getSettings();
+        servo.remote_getServoSpeed();
+        servo.remote_getServoStatus();
+    
+    }
+    
+    for (CurrentSensor current :this.currentSensors)
+    {
+        current.remote_getSettings();
+    }
 
-	
-	for (Lm75 lm75 :this.temperatureSensors)
-	{
-		lm75.remote_getSettings();
-	}
-	
+    
+    for (Lm75 lm75 :this.temperatureSensors)
+    {
+        lm75.remote_getSettings();
+    }
+    
 }
 
 
@@ -180,7 +171,7 @@ public void loadSetup()
 
 public LegControllerLm75 getTemperatureSensors()
 {
-	return (this.temperatureSensors);
+    return (this.temperatureSensors);
 }
 
 
