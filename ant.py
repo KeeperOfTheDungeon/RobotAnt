f5dc5e563a6cb98a7735bdcdf33214a1fc5234db
ermu1013 <ermu1013@gmail.com>
2023-09-17 15:42:12 +0200

    refactor various files

--- ant.py
+++ ant.py
@@ -8,6 +8,7 @@ from RoboControl.Com.Connection.Connection import Connection
 from RoboControl.Com.Connection.SerialConnection import SerialConnection
 from Devices.LegSensors.LegSensors import LegSensors
 from RoboControl.Robot.Device.Generic.DataHub.DataHub import DataHub
+from RoboControl.Robot.Device.RobotDevice import RobotDevice
 from RoboControl.Robot.Robot import Robot
 
 
@@ -33,22 +34,34 @@ class Ant(Robot):
         super().connect(connection)
         self.add_devices()
 
+    def add_device(self, device: RobotDevice) -> None:
+        self._device_list.append(device)
+        device.set_transmitter(self._connection)
+
     def add_devices(self):
         self._data_hub = DataHub(AntDeviceConfig.MAIN_DATA_HUB)
-        self._device_list.append(self._data_hub)
-        self._data_hub.set_transmitter(self._connection)
+        self.add_device(self._data_hub)
 
         self._head_sensors = HeadSensors(AntDeviceConfig.HEAD_SENSORS)
-        self._device_list.append(self._head_sensors)
-        self._head_sensors.set_transmitter(self._connection)
+        self.add_device(self._head_sensors)
 
         self._leg_sensors = LegSensors(AntDeviceConfig.LEG_SENSORS)
-        self._device_list.append(self._leg_sensors)
-        self._leg_sensors.set_transmitter(self._connection)
+        self.add_device(self._leg_sensors)
 
         self._leg_controller = LegController(AntDeviceConfig.LEG_CONTROLLER)
-        self._device_list.append(self._leg_controller)
-        self._leg_controller.set_transmitter(self._connection)
+        self.add_device(self._leg_controller)
+
+        # self._tail_board = TailBoard(AntDeviceConfig.TAIL_BOARD)
+        # self.add_device(self._tail_board)
+
+        # self._ir_com = IrCom(AntDeviceConfig.IR_COM)
+        # self.add_device(self._ir_com)
+
+        # self._pixy_controller = PixyController(AntDeviceConfig.PIXY_CONTROLLER)
+        # self.add_device(self._pixy_controller)
+
+        # self._behavior = AntBehavior(self)
+        # self.add_device(self._behavior)
 
     def get_head_sensors(self):
         return self._head_sensors
@@ -61,27 +74,3 @@ class Ant(Robot):
 
     def get_data_hub(self):
         return self._data_hub
-
-
-"""
-#	leg = new LegController(AntDeviceId.LEG_CONTROLLER.getDeviceMetaData());    
-#	this.deviceList.add(leg);
-    
-#	this.addMainHub(AntComponents.ROBOT_NAME.getMetaData());
-    
-
-    
-    
-    
-    
-    
-#	this.deviceList.add(new HeadSensors(AntDeviceId.HEAD_SENSORS.getDeviceMetaData()));
-#	this.deviceList.add(new TailBoard(AntDeviceId.TAIL_BOARD.getDeviceMetaData()));
-#	this.deviceList.add(new LegSensors(AntDeviceId.LEG_SENSORS.getDeviceMetaData()));
-    
-#	this.deviceList.add(new IrCom(AntDeviceId.IR_COM.getDeviceMetaData()));
-    
-#	this.deviceList.add(new PixyController(AntDeviceId.PIXY_CONTROLLER.getDeviceMetaData()));
-    
-#	this.behavior = new  AntBehavior(this);
-"""
